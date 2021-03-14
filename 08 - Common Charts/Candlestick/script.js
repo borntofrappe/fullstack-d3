1f623cbdf638bde42910382a77af20cf0c2de5f5
borntofrappe <borntofrappe@gmail.com>
2021-03-14 23:59:36 +0100

    code | candlestick chart

---
+++ 08 - Common Charts/Candlestick/script.js
@@ -0,0 +1,19 @@
+async function drawCandlestickChart() {
+  const data = [
+    { date: '2020-11-23',open: 83.55, close: 83.62, low: 83.38, high: 83.88 },
+    { date: '2020-11-24',open: 82.65, close: 83.36, low: 82.65, high: 83.7 },
+    { date: '2020-11-25',open: 82.75, close: 83.44, low: 82.69, high: 83.7 },
+    { date: '2020-11-26',open: 83.2, close: 82.92, low: 82.87, high: 83.39 },
+    { date: '2020-11-27',open: 83.98, close: 83.49, low: 83.47, high: 24.49 },
+    { date: '2020-11-30',open: 83.55, close: 83.62, low: 83.38, high: 83.88 },
+    { date: '2020-11-31',open: 83.88, close: 83.25, low: 83.24, high: 83.93 },
+    { date: '2020-12-01',open: 83.17, close: 83.25, low: 82.85, high: 83.4 },
+    { date: '2020-12-02',open: 82.65, close: 83.36, low: 82.65, high: 83.7 },
+    { date: '2020-12-03',open: 83.2, close: 82.92, low: 82.87, high: 83.39 },
+    { date: '2020-12-06',open: 83.03, close: 82.97, low: 82.44, high: 83.15 },
+    { date: '2020-12-07',open: 82.75, close: 83.44, low: 82.69, high: 83.7 },
+  ];
+
+}
+
+drawCandlestickChart();
