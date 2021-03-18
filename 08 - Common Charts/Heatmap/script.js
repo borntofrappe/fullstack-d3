3376902ed2616862b8b360236a9aac71ffa7c2db
borntofrappe <borntofrappe@gmail.com>
2021-03-18 16:20:38 +0100

    code | heatmap

---
+++ 08 - Common Charts/Heatmap/script.js
@@ -0,0 +1,242 @@
+async function drawHeatmap() {
+  /* ACCESS DATA */
+  const data = await d3.json('../../nyc_weather_data.json');
+  const dateParser = d3.timeParse('%Y-%m-%d');
+  const dateAccessor = d => dateParser(d.date);
+
+  const dataset = data.sort((a, b) => dateParser(b) - dateParser(a))
+  const firstDate = dateAccessor(dataset[0]);
+  const weekAccessor = d => d3.timeWeeks(firstDate, dateAccessor(d)).length;
+  const dayAccessor = d => d3.timeFormat("%w")(dateAccessor(d))
+
+  const weeks = weekAccessor(dataset[dataset.length - 1])
+
+  const dayParser = d3.timeParse('%e') //  1 2 3 4 5
+  const dayFormatter = d3.timeFormat('%A') // Monday
+
+  const monthFormatter = d3.timeFormat('%b') // Jan
+
+const daysOfWeek = Array(7)
+.fill()
+.map((d, i) => dayFormatter(dayParser(i)))
+
+const months = d3
+.timeMonths(dateAccessor(dataset[0]), dateAccessor(dataset[dataset.length - 1]))
+
+
+  /* CHART DIMENSIONS */
+  const dimensions = {
+    width: 800,
+    margin: {
+      top: 20,
+      right: 0,
+      bottom: 0,
+      left: 80,
+    },
+  };
+
+  dimensions.boundedWidth =
+    dimensions.width - (dimensions.margin.left + dimensions.margin.right);
+  
+  const tileTotalSize = dimensions.boundedWidth / (weeks + 1);
+  const tilePadding = 0;
+  const tileSize = tileTotalSize - tilePadding * 2;
+  
+  dimensions.height = tileTotalSize * daysOfWeek.length + (dimensions.margin.top + dimensions.margin.bottom);
+
+  dimensions.boundedHeight =
+    dimensions.height - (dimensions.margin.top + dimensions.margin.bottom);
+
+  const dimensionsLegend = {
+    width: 220,
+    height: 30,
+    margin: {
+      top: 5,
+      right: 40,
+      bottom: 5,
+      left: 40,
+    },
+  };
+
+  dimensionsLegend.boundedWidth =
+    dimensionsLegend.width - (dimensionsLegend.margin.left + dimensionsLegend.margin.right);
+  dimensionsLegend.boundedHeight =
+    dimensionsLegend.height - (dimensionsLegend.margin.top + dimensionsLegend.margin.bottom);
+
+
+  /* SCALES */
+  const interpolateColor = d3.interpolateBlues;
+  const colorScale = d3.scaleLinear()
+    .range([0, 1]);
+
+
+
+  /* DRAW DATA */
+  const heatmapMetric = d3.select('#wrapper').append('h2')
+  const heatmapGradientId = 'heatmap-gradient';
+
+  const heatmapLegend = d3
+    .select('#wrapper')
+    .append('svg')
+    .attr('id', 'legend')
+    .attr('width', dimensionsLegend.width)
+    .attr('height', dimensionsLegend.height);
+
+  heatmapLegend
+    .append('linearGradient')
+    .attr('id', heatmapGradientId)
+    .selectAll('stop')
+    .data([0, 1])
+    .enter()
+    .append('stop')
+    .attr('offset', (d, i, {length}) => `${i * 100 / (length - 1)}%`)
+    .attr('stop-color', d => interpolateColor(d))
+
+  const heatmapGroup = heatmapLegend
+    .append('g')
+    .style(
+      'transform',
+      `translate(${dimensionsLegend.margin.left}px, ${dimensionsLegend.margin.top}px)`
+    );
+
+  heatmapGroup
+      .append('rect')
+      .attr('width', dimensionsLegend.boundedWidth)
+      .attr('height', dimensionsLegend.boundedHeight)
+      .attr('fill', `url(#${heatmapGradientId})`);
+  
+  const heatmapMin = heatmapGroup
+    .append('text')
+    .attr('text-anchor', 'end')
+    .attr('dominant-baseline', 'middle')
+    .attr('x', -3)
+    .attr('y', dimensionsLegend.boundedHeight / 2)
+    .attr('font-size', 13)
+
+    const heatmapMax = heatmapGroup
+    .append('text')
+    .attr('text-anchor', 'start')
+    .attr('dominant-baseline', 'middle')
+    .attr('x', dimensionsLegend.boundedWidth + 3)
+    .attr('y', dimensionsLegend.boundedHeight / 2)
+    .attr('font-size', 13)
+
+  const heatmapButton = d3
+    .select('#wrapper')
+    .append('button')
+    .text('Change metric');
+
+  const wrapper = d3
+    .select('#wrapper')
+    .append('svg')
+    .attr('id', 'heatmap')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height);
+
+  const bounds = wrapper
+    .append('g')
+    .style(
+      'transform',
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
+
+  const axisGroup = bounds.append('g');
+  const metricGroup = bounds.append('g');
+
+  
+  /* PERIPHERALS */
+  const xAxisGroup = axisGroup.append('g');
+  const yAxisGroup = axisGroup.append('g');
+
+  xAxisGroup
+      .selectAll('text')
+      .data(months)
+      .enter()
+      .append('text')
+      .text(d => monthFormatter(d))
+      .attr('x', d => {
+        return d3.timeWeeks(firstDate, d).length * tileTotalSize;
+      }) 
+      .attr('y', -dimensions.margin.top + 12)
+
+  
+    yAxisGroup
+      .selectAll('text')
+      .data(daysOfWeek)
+      .enter()
+      .append('text')
+      .text(d => d)
+      .attr('text-anchor', 'end') 
+      .attr('dominant-baseline', 'middle') 
+      .attr('x', -5) 
+      .attr('y', (d, i) => i * tileTotalSize + 6)
+
+  
+
+  axisGroup
+    .selectAll('text')
+    .attr('font-size', 12)
+    .attr('fill', 'currentColor');
+
+  function drawMap(metric) {
+    const metricAccessor = d => d[metric]
+    colorScale
+      .domain(d3.extent(dataset, metricAccessor));
+
+
+    heatmapMetric.text(metric);
+    heatmapMin
+      .text(d3.min(dataset, metricAccessor))
+    
+    heatmapMax 
+      .text(d3.max(dataset, metricAccessor))
+
+    const updateGroup = metricGroup
+      .selectAll('rect')
+      .data(dataset);
+
+    const enterGroup = updateGroup
+      .enter()
+
+    const exitGroup = updateGroup
+      .exit();
+
+    exitGroup.remove();
+
+    enterGroup
+      .append('rect')
+      .attr('fill', d => interpolateColor(colorScale(metricAccessor(d))))
+      .attr('x', (d) => weekAccessor(d) * tileTotalSize + tilePadding)
+      .attr('y', (d) => dayAccessor(d) * tileTotalSize + tilePadding)
+      .attr('width', tileSize)
+      .attr('height', tileSize);
+
+    updateGroup
+      .transition()
+      .attr('fill', d => interpolateColor(colorScale(metricAccessor(d))))
+  }
+
+  const metrics = [
+    'windSpeed',
+    'moonPhase',
+    'dewPoint',
+    'humidity',
+    'uvIndex',
+    'windBearing',
+    'temperatureMin',
+    'temperatureMax',
+  ];
+  let metricIndex = 0;
+
+
+  drawMap(metrics[metricIndex]);
+  
+  heatmapButton
+  .on('click', () => {
+    metricIndex = (metricIndex + 1) % metrics.length;
+    drawMap(metrics[metricIndex]);
+  })
+  
+}
+
+drawHeatmap();
